b2f827b84c15446ff284e6fbdbfb1851ac53fbd3
Baha Eren YALDIZ <beyaldiz17@gmail.com>
2021-11-23 21:12:02 +0900

    Update augmentable class

--- datasets/augmentable.py
+++ datasets/augmentable.py
@@ -5,23 +5,83 @@ import torchvision.transforms as transforms
 
 
 class AugmentableDataset(Dataset):
-    def __init__(self, images, targets):
+    def __init__(self, images, targets, transformations, pre_transform=None, post_transform=None):
         """
+        Images and targets are passed as any type.
+        Pre transform is applied in the beginning of getitem.
+        Transformations passed in self.transforms applied in the middle (i.e augmentations from the genomes).
+        Post transform is applied in the end of getitem.
+
         Args:
-            images (Tensor): Tensor of shape (N, C, H, W)
-            targets (Tensor): Tensor of shape (N, )
+            images (Any)
+            targets (Any)
+            transformations (Transformations)
         """
         self.images = images
         self.targets = targets
-        self.transformed_images = images.clone()
-        self.transforms = [None for _ in range(self.images[0])]
+        self.transformations = transformations
+        self.pre_transform = pre_transform
+        self.post_transform = post_transform
+        self.transforms = [None for _ in range(self.images.shape[0])]
+        self._eval_children = False
+        self._children = None
     
     def __len__(self):
-        return len(self.images.shape[0])
+        return self.images.shape[0]
     
     def __getitem__(self, idx):
-        return self.transformed_images[idx], self.targets[idx]
+        if self._eval_children:
+            instances = []
+            for genome in self._children[idx]:
+                tr_array = []
+                if self.pre_transform != None:
+                    tr_array.append(self.pre_transform)
+                transform = self.transformations.get_transformation(genome)
+                tr_array.append(transform)
+                if self.post_transform != None:
+                    tr_array.append(self.post_transform)
+                tr = transforms.Compose(tr_array)
+                instances.append(tr(self.images[idx]))
+            return instances, [self.targets[idx] for _ in range(len(instances))]
+
+        else:
+            tr_array = []
+            if self.pre_transform != None:
+                tr_array.append(self.pre_transform)
+            if self.transforms[idx] != None:
+                tr_array.append(self.transforms[idx])
+            if self.post_transform != None:
+                tr_array.append(self.post_transform)
+            if len(tr_array) != 0:
+                tr = transforms.Compose(tr_array)
+                return tr(self.images[idx]), self.targets[idx]
+            else:
+                return self.images[idx], self.targets[idx]
+
+    def pick_best_child(self, f_best):
+        """
+        f_best is a list of indices that correspond to the children that have the largest loss
+
+        Args:
+        f_best (numpy.array, shape: (N, )): indices of the children that have the largest loss 
+        """
+        for i in range(f_best.shape[0]):
+            self.transforms[i] = self.transformations.get_transformation(self._children[i][f_best[i]])
     
-    def update_transform(self, idx, transform):
-        self.transforms[idx] = transform
-        self.transformed_images[idx] = transform(self.images[idx])
+    def eval_children(self):
+        """
+        Switching to evaluation of children's mode
+        """
+        self._eval_children = True
+    
+    def train_best(self):
+        """
+        Switching to training with the best children mode
+        """
+        self._eval_children = False
+    
+    def update_children(self, children):
+        """
+        Updates the children array
+        """
+        self._children = children
