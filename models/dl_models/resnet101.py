e2ccbfafad7dbd95a3116d63cc07be8a8677a881
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-12-06 09:43:18 +0700

    fix: CPU compatibility

--- models/dl_models/resnet101.py
+++ models/dl_models/resnet101.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 
 
 class ResNet101(nn.Module):
-    def __init__(self):
+    def __init__(self, cuda=True):
         super().__init__()
-
+        self.cuda = cuda
         self.model = torch.hub.load('pytorch/vision:v0.10.0',
                                     'resnet101',
                                     pretrained=False)
 
     def forward(self, x):
-        return self.model(x.cuda())
+        if self.cuda:
+            return self.model(x.cuda())
+        else:
+            return self.model(x)
